e035e8731b9a27f0027edf1d81fb23700e917612
Karin Karlsson <karin.drake@gmail.com>
2024-03-20 10:33:48 +0100

    Add retrofont and create layout css file

---
+++ app/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, DotGothic16  } from "next/font/google";
+
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const retroFont = DotGothic16({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap'
+})
