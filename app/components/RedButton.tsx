194b6a66af30e33c489f4e50030b43ebb5000f5c
Karin Karlsson <karin.drake@gmail.com>
2024-03-21 09:47:10 +0100

    Creatae redbutton component

---
+++ app/components/RedButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './styles/redButton.module.css'
+
+
+const RedButton: React.FC = ()=> {
+
+    return <button className={styles.redButton}>Get users</button>
+}
+
+export default RedButton
