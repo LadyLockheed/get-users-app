570f8f4fcc30fc6956fa16d5ed62d0db6718a2a4
Karin Karlsson <karin.drake@gmail.com>
2024-03-21 18:58:38 +0100

    Add error property, cleaning up a bit

--- app/UserView/page.tsx
+++ app/UserView/page.tsx
@@ -5,42 +5,44 @@ import RedButton from '../components/RedButton'
 import GreyButtons from '../components/GreyButtons'
 import Screen from '../components/Screen'
 import axios from 'axios'
-import styles from './userView.module.css'
 
-const UserView = ()=> {
+const UserView: React.FC = ()=> {
 
   const [user, setUser] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [hasError, setHasError] = useState(false)
 
     const fetchUser = async()=> {
  
         setIsLoading(true)
-      try {
-        const response = await axios.get('https://randomuser.me/api/')
-        if(!response) {
-          console.log('somehting is wrong')
-          
+
+        try {
+
+            const response = await axios.get('https://randomuser.me/api/')
+
+            if(!response) {
+                setHasError(true)
+            }
+            else {
+            setUser(response.data.results)
+            }
+
         }
-        else {
-          setUser(response.data.results)
-         
+        catch(error) {
+            setHasError(true)
         }
-      }
-      catch(error) {
-        console.log(error)
-      }
-      finally {
-        setIsLoading(false)
-      }
+        finally {
+            setIsLoading(false)
+        }
     }
 
     return (
-    <>  
-      <Screen isLoading={isLoading} user={user}/>
-      <CrossButton/>
-      <RedButton getUser={fetchUser}/>
-      <GreyButtons/>
-    </>
+        <>  
+            <Screen isLoading={isLoading} user={user} hasError={hasError}/>
+            <CrossButton/>
+            <RedButton getUser={fetchUser}/>
+            <GreyButtons/>
+        </>
     )
 }
 
