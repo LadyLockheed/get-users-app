6f5c4f7130b9c8197676009a952090e7e2c52a87
Karin Karlsson <karin.drake@gmail.com>
2024-03-21 10:47:41 +0100

    Send fetchuser func as prop

--- app/components/RedButton.tsx
+++ app/components/RedButton.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import styles from './styles/redButton.module.css'
 
+interface RedButtonProps {
 
-const RedButton: React.FC = ()=> {
+    getUser: ()=>void
+}
+const RedButton: React.FC<RedButtonProps> = ({getUser})=> {
 
-    return <button className={styles.redButton}>Get users</button>
+    return <button className={styles.redButton} onClick={()=> getUser()}>Get users</button>
 }
 
 export default RedButton
