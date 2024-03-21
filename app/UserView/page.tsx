26cd48c60db785a541e13193a45a8a452489575f
Karin Karlsson <karin.drake@gmail.com>
2024-03-21 10:55:38 +0100

    Create userview and fetchfunction

---
+++ app/UserView/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+import React, { useState } from 'react'
+import CrossButton from '../components/CrossButton'
+import RedButton from '../components/RedButton'
+import SelectAndStartButton from '../components/SelectAndStartButton'
+import Screen from '../components/Screen'
+import axios from 'axios'
+import styles from './userView.module.css'
+
+const UserView = ()=> {
+
+  const [user, setUser] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+    const fetchUser = async()=> {
+ 
+        setIsLoading(true)
+      try {
+        const response = await axios.get('https://randomuser.me/api/')
+        if(!response) {
+          console.log('somehting is wrong')
+          
+        }
+        else {
+          setUser(response.data.results)
+         
+        }
+      }
+      catch(error) {
+        console.log(error)
+      }
+      finally {
+        setIsLoading(false)
+      }
+    }
+
+    return (
+    <>  
+      <Screen isLoading={isLoading} user={user}/>
+      <CrossButton/>
+      <RedButton getUser={fetchUser}/>
+      <SelectAndStartButton/>
+    </>
+    )
+}
+
+export default UserView
